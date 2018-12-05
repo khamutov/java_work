c9f7b30df6db5038a97ff8408f4ad506a220c8b6
S.Miroshin <s.miroshin@butik.ru>
2018-12-05 12:51:28 +0300

    Add test  for size,toArray,remove

--- src/test/java/com/github/miroshinsv/learning/collection/CustomArrayTest.java
+++ src/test/java/com/github/miroshinsv/learning/collection/CustomArrayTest.java
@@ -31,4 +31,54 @@ public class CustomArrayTest {
         instance.add(null);
         assertTrue(instance.contains(null));
     }
+
+    @Test
+    public void testCheckArray(){
+        instance.add(1);
+        assertEquals(instance.size(),1);
+    }
+
+    @Test
+    public void testCheckArraySeveral(){
+        instance.add(1);
+        instance.add(2);
+        assertEquals(instance.size(),2);
+    }
+
+    @Test
+    public void testIsEmpty(){
+        assertTrue(instance.isEmpty());
+    }
+
+    @Test
+    public void testIsNotEmpty(){
+        instance.add(1);
+        assertFalse(instance.isEmpty());
+    }
+
+    @Test
+    public void testToArray(){
+        instance.add(1);
+        instance.add(2);
+        instance.add(3);
+        assertEquals(new Object[]{1,2,3},instance.toArray());
+    }
+
+    @Test
+    public void testRemove(){
+        instance.add(1);
+        instance.add(2);
+        instance.add(3);
+        instance.remove((Object) 1);
+        assertEquals(new Object[]{2,3},instance.toArray());
+    }
+
+    @Test
+    public void testRemoveNull(){
+        instance.add(1);
+        instance.add(2);
+        instance.add(3);
+        instance.remove(null);
+        assertEquals(new Object[]{1,2,3},instance.toArray());
+    }
 }
